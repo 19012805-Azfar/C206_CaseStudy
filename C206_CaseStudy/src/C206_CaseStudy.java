06279e4e909b7e83980a7a828f4970ff5a7a3a8e
19032227 <19032227@DESKTOP-H5G3U56>
2020-08-19 15:41:22 +0800

    This is for the Item part

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -3,7 +3,33 @@ public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		UserDB.showMenu();
+		int option = 0;
+		while (option != 4) {
+			Helper.line(30, "=");
+			System.out.println("Choose one");
+			Helper.line(30, "=");
+			System.out.println("1. Users");
+			System.out.println("2. Item");
+			System.out.println("3. Item");
+			System.out.println("4. Exit");
+			
+			option = Helper.readInt("Select option > ");
+			
+			if (option == 1) {
+				UserDB.showMenu();
+			}
+			else if (option == 2) {
+				ItemDB.showMenu();
+			}
+			else {
+				System.out.println("End");
+				break;
+			}
+	
+		
+		
 	}
+
+}
 
 }
